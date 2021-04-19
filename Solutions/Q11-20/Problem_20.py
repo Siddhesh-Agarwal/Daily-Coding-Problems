8719650d2c4339598050eb632533ef0fc3d97eec
Siddhesh Agarwal <68057995+Siddhesh-Agarwal@users.noreply.github.com>
2021-04-19 09:35:23 +0530

    Update Problem_20.py

--- Solutions/Q11-20/Problem_20.py
+++ Solutions/Q11-20/Problem_20.py
@@ -1,4 +1,6 @@
 '''
+This problem was asked by Google.
+
 Given two singly linked lists that intersect at some point, find the intersecting node. The lists are non-cyclical.
 
 For example, given A = 3 -> 7 -> 8 -> 10 and B = 99 -> 1 -> 8 -> 10, return the node with value 8.
